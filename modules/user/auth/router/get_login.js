5b540663b37c6358014bc9068902b72f1f3f424a
ciwan <cokyamanmuhammet@gmail.com>
2022-03-03 00:02:55 +0300

    fixed

--- modules/user/auth/router/get_login.js
+++ modules/user/auth/router/get_login.js
@@ -1,6 +1,6 @@
 
 const route = async(req,res,next) => {
-    return res.status(200).send({ status: true, message: "User Login Page"})
+    return res.status(200).send({ status: true, message: "User Login Page Success"})
 }
 
 module.exports = route
