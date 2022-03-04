9a4dfbcb3f89432f0b84fb261af8b594bd808bc2
ciwan <cokyamanmuhammet@gmail.com>
2022-03-04 14:50:45 +0300

    new

--- middlewares/authJwt.js
+++ middlewares/authJwt.js
@@ -4,17 +4,17 @@ const {TokenExpiredError} = jwt;
 
 const catchError = (err,res) => {
     if( err instanceof TokenExpiredError)
-        return res.status(401).send({ message: "A_Unauthorized! Access Token was expired!" })
-    return res.sendStatus(401).send({ message: "A_Unauthorized !" });
+        return res.status(401).send({ message: "A_Unauthorized! Access Token was expired!" , data: null })
+    return res.sendStatus(401).send({ message: "A_Unauthorized !", data: null });
 }
 
 const route = async (req,res,next) => {
     let auth = req.header("Authorization");
     if(!auth)
-        return res.status(401).send({ status: false, message: "A_Unauthorized_1"});
+        return res.status(401).send({ status: false, message: "A_Unauthorized_1", data: null });
     auth = auth.split(" ")[1];
     if(!auth)
-        return res.status(401).send({ status: false, message: "A_Unauthorized_2"});
+        return res.status(401).send({ status: false, message: "A_Unauthorized_2", data: null });
     await jwt.verify(auth, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
         if (err)
             return catchError(err,res)
