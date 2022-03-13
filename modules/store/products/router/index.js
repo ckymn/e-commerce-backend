f41070f8970b8f71ccfa08d9b789f36d2c394695
ciwan <cokyamanmuhammet@gmail.com>
2022-03-13 14:17:33 +0300

    new

--- modules/store/products/router/index.js
+++ modules/store/products/router/index.js
@@ -3,5 +3,6 @@ module.exports = {
     single_product : require("./single_product"),
     add_products: require("./add_products"),
     delete_product: require("./delete_product"),
-    update_product: require("./update_product")
+    update_product: require("./update_product"),
+    search_product: require("./search_product")
 }
